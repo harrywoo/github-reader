8bb2912dc25a7be8ba9d68e382283aa7e3c65681
Harry <harryhz@yahoo.com>
2023-11-15 01:01:00 +0800

    add return count

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def parse_json(json_data):
         if isinstance(info_list, list) and len(info_list) >= 3:
             results = []
             # 遍历前三个元素
-            for index, item in enumerate(info_list[:3]):
+            for index, item in enumerate(info_list[:5]):
                 # 检查所需的字段是否存在
                 if 'content' in item and 'title' in item:
                     # 拼接title和content
@@ -25,6 +25,34 @@ def parse_json(json_data):
     else:
         return "JSON structure does not match expected format."
 
+# 假设这是您提供的解析JSON的函数
+def parse_json_to_url(json_data):
+    # 首先检查是否存在所需的路径
+    if  'data' in json_data and 'info' in json_data['data']:
+        info_list = json_data['data']['info']
+        # 确保info_list是一个列表并且有至少三个元素
+        if isinstance(info_list, list) and len(info_list) >= 3:
+            results = []
+            # 遍历前三个元素
+            for index, item in enumerate(info_list[:6]):
+                # 检查所需的字段是否存在
+                if 'content' in item and 'title' in item:
+                    # 拼接title和content
+                    result = "下面是搜索结果【" + str(index) +"】"+ "title:" + item['title'] + "content: " + item['content']
+                    results.append(result)
+            # 将拼接的结果作为字符串返回
+            return "\n".join(results)
+        else:
+            return "Info list is not valid or does not have enough elements."
+    else:
+        return "JSON structure does not match expected format."
+
+
+@app.get("/")
+async def get_main():
+    return {"message": "Welcome to Code Reader!"}
+
+
 @app.get("/search/")
 async def search(query: str):
     url = "https://t.aliyun.com/abs/search/searchHelpDocv?queryWord={}&limit=20&pageNo=1&from=pc&categoryId=&loc=search_helpdoc_item".format(query)
@@ -36,6 +64,20 @@ async def search(query: str):
         return result
     except requests.RequestException as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get("/searchDetail/")
+async def search(query: str):
+    url = "https://t.aliyun.com/abs/search/searchHelpDocv?queryWord={}&limit=20&pageNo=1&from=pc&categoryId=&loc=search_helpdoc_item".format(query)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # 确保请求成功
+        json_data = response.json()
+        result = parse_json(json_data)
+        return result
+    except requests.RequestException as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
 
 if __name__ == "__main__":
     import uvicorn
