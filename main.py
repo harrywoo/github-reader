ce8d42126faaaf569a1cc0f0525e67c952e8a5f5
Harry <harryhz@yahoo.com>
2023-11-14 23:29:41 +0800

    initial commit

--- main.py
+++ main.py
@@ -1,91 +1,42 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import os
-import shutil
-import asyncio
-from git import Repo
+import requests
 
 app = FastAPI()
 
+# 假设这是您提供的解析JSON的函数
+def parse_json(json_data):
+    # 首先检查是否存在所需的路径
+    if  'data' in json_data and 'info' in json_data['data']:
+        info_list = json_data['data']['info']
+        # 确保info_list是一个列表并且有至少三个元素
+        if isinstance(info_list, list) and len(info_list) >= 3:
+            results = []
+            # 遍历前三个元素
+            for index, item in enumerate(info_list[:3]):
+                # 检查所需的字段是否存在
+                if 'content' in item and 'title' in item:
+                    # 拼接title和content
+                    result = "【" + str(index) +"】"+ "title:" + item['title'] + "content: " + item['content']
+                    results.append(result)
+            # 将拼接的结果作为字符串返回
+            return "\n".join(results)
+        else:
+            return "Info list is not valid or does not have enough elements."
+    else:
+        return "JSON structure does not match expected format."
 
-class GitRepo(BaseModel):
-    git_url: str
-
-
-@app.get("/")
-async def get_main():
-    return {"message": "Welcome to Code Reader!"}
-
-
-@app.post("/get-repo-content/")
-async def print_repo_url(repo: GitRepo):
-    git_url = repo.git_url
+@app.get("/search/")
+async def search(query: str):
+    url = "https://t.aliyun.com/abs/search/searchHelpDocv?queryWord={}&limit=20&pageNo=1&from=pc&categoryId=&loc=search_helpdoc_item".format(query)
     try:
-        repo_name = git_url.split("/")[-1]
-        repo_name = (
-            repo_name.replace(".git", "") if repo_name.endswith(".git") else repo_name
-        )
-
-        temp_dir = f"./temp_{repo_name}"
-        if os.path.exists(temp_dir):
-            shutil.rmtree(temp_dir)
-        os.makedirs(temp_dir)
-
-        repo_dir = os.path.join(temp_dir, repo_name)
-        print("start cloning")
-        # 异步克隆仓库
-        await clone_repo_async(git_url, repo_dir)
-
-        print("start reading")
-        # 异步读取所有文件
-        content = await read_all_files_async(repo_dir)
-        print("read finished. conetent length: ", len(content))
-        # 确保在此处删除临时目录
-        shutil.rmtree(temp_dir)
-        print("temp dir removed")
-        return {"content": content[:50000]}
-
-    except Exception as e:
-        # 如果出现异常，也应该清理临时目录
-        if os.path.exists(temp_dir):
-            shutil.rmtree(temp_dir)
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-async def clone_repo_async(git_url, repo_dir):
-    loop = asyncio.get_event_loop()
-    print(f"开始克隆仓库: {git_url}")
-    await loop.run_in_executor(
-        None, lambda: Repo.clone_from(git_url, repo_dir, depth=1)
-    )
-    print(f"仓库克隆完成: {repo_dir}")
-
-
-async def read_all_files_async(directory):
-    loop = asyncio.get_event_loop()
-    print(f"开始读取文件: {directory}")
-    content = await loop.run_in_executor(None, lambda: read_all_files(directory))
-    print(f"文件读取完成. 总字符数: {len(content)}")
-    return content
-
-
-def read_all_files(directory):
-    all_text = ""
-    for root, dirs, files in os.walk(directory):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            if os.path.isfile(file_path):
-                try:
-                    with open(file_path, "r", encoding="utf-8") as file:
-                        all_text += f"File: {file_name}\n\n" + file.read() + "\n\n"
-                except UnicodeDecodeError:
-                    print(f"无法以UTF-8编码读取文件: {file_path}")
-                except Exception as e:
-                    print(f"读取文件时发生错误: {file_path}, 错误: {e}")
-    return all_text
-
+        response = requests.get(url)
+        response.raise_for_status()  # 确保请求成功
+        json_data = response.json()
+        result = parse_json(json_data)
+        return result
+    except requests.RequestException as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("main:app", host="0.0.0.0", port=80)
